a77d5112a748a3281a8b07fc451b77ffe8a2a330
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-02-16 16:57:58 -0300

    test: ensure RemoveAuthentication calls HttpPostClient with correct body

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,7 @@ import { faker } from "@faker-js/faker";
 
 import { RemoveAuthentication } from "./remote-authentication";
 import { HttpPostClientSpy } from "../../test/mock-http-client";
+import { mockAuthentication } from "../../../domain/test/mock-authentication";
 
 type SutTypes = {
   sut: RemoveAuthentication;
@@ -22,8 +23,16 @@ describe("RemoteAuthentication", () => {
   test("should call HttpPostClient with correct URL", async () => {
     const url = faker.internet.url();
     const { httpPostClientSpy, sut } = makeSut(url);
-    await sut.auth();
+    await sut.auth(mockAuthentication());
 
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  test("should call HttpPostClient with correct body", async () => {
+    const { httpPostClientSpy, sut } = makeSut();
+    const authenticationParams = mockAuthentication();
+    await sut.auth(authenticationParams);
+
+    expect(httpPostClientSpy.body).toEqual(authenticationParams);
   });
 });
