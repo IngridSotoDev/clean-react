7fc572ece0d1b5d5e51b7c13b49646e7d9b3527f
ingrid soto <ingrid.oliveira@aiqfome.com>
2023-02-16 19:08:31 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,13 +1,8 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client";
-import { HttpStatusCode } from "data/protocols/http/http-response";
+import { HttpPostClient, HttpStatusCode } from "data/protocols/http";
 
-import { UnexpectedError } from "domain/errors/unexpected-error";
-import {
-  Authentication,
-  AuthenticationParams,
-} from "domain/usecases/authentication";
-import { InvalidCredentialsError } from "domain/errors/invalid-credentials-error";
-import { AccountModel } from "domain/models/account-model";
+import { AccountModel } from "domain/models";
+import { Authentication, AuthenticationParams } from "domain/usecases";
+import { UnexpectedError, InvalidCredentialsError } from "domain/errors";
 
 export class RemoveAuthentication implements Authentication {
   constructor(
